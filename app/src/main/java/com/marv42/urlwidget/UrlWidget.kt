c4a9b30196af092097c0a8f61b74dcd1caac91e6
Marvin <marv42+github@gmail.com>
2025-02-21 23:29:36 +0100

    Initial commit

---
+++ app/src/main/java/com/marv42/urlwidget/UrlWidget.kt
@@ -0,0 +1,39 @@
+package com.marv42.urlwidget
+
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+
+
+class UrlWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val uri = Uri.parse("https://ams.lu/Tools/TheSameZuko.ams/5/Halle")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, FLAG_IMMUTABLE)
+
+        val widgetText = context.getString(R.string.appwidget_text)
+        val remoteViews = RemoteViews(context.packageName, R.layout.url_widget)
+//        remoteViews.setTextViewText(R.id.appwidget_text, widgetText)
+        remoteViews.setOnClickPendingIntent(R.id.appwidget, pendingIntent)
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+    }
+}
